8ed6dd362c1efd57a7b7950bf7b330b5dd1ba652
Rodrigo <rodriigosantos11@gmail.com>
2022-09-02 01:55:12 -0300

    port fix

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ io.on('connection', (socket) => {
 })
 
 const PORT = process.env.PORT || 3000;
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
 });
